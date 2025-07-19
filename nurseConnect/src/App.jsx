e8f74b60443dc8d19d0e360b1a47ffc801e073e3
Barno June <barnoj21@gmail.com>
2025-07-19 19:19:22 +0300

    Init

---
+++ nurseConnect/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+    <p className="text-2xl font-bold text-center mt-10 text-blue-600">
+      Welcome to Nurse Connect!
+    </p>
+    </>
+  )
+}
+
+export default App
