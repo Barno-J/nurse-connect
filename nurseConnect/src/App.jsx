45394535d56e011c3ab470f29e5b45612211932d
Fromb23 <rombo.programming@gmail.com>
2025-07-26 16:18:41 -0400

    Added JobDescription route to router
    - Registered /jobdescription route pointing to JobDescription component
    - Ensured nested routes like /dashboard/* continue to function
    - Keeps routing consistent with new job details modal design

--- nurseConnect/src/App.jsx
+++ nurseConnect/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Dashboard from './components/dashboard/Dashboard';
+import JobDescription from './components/JobDescription';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
+        <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/jobdescription" element={<JobDescription />} />
       </Routes>
     </Router>
   );
