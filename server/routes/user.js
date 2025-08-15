a0fc004a59b7192ccde63432aa72f9a8a289c955
Fromb23 <rombo.programming@gmail.com>
2025-08-15 04:52:58 -0400

    feat(api): add user authentication routes
    - Added POST /login endpoint for user login
    - Added POST /register endpoint for user registration
    - Linked routes to corresponding controller methods

---
+++ server/routes/user.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { loginUser, createUser } from '../controllers/user.js';
+
+const router = express.Router();
+
+router.post('/login', loginUser);
+router.post('/register', createUser);
+
+export default router;
