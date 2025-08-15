c46a4e6aadd20c770f075dd0a3764a012c10fde6
Fromb23 <rombo.programming@gmail.com>
2025-08-15 04:25:27 -0400

    refactor(store): add jobReducer to Redux store configuration
    - Imported jobReducer from slices/jobSlice
    - Registered job reducer in store to manage job-related state
    - Improved store structure for better state organization

--- nurseConnect/src/redux/store.js
+++ nurseConnect/src/redux/store.js
@@ -1,10 +1,12 @@
 
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './slices/userSlice';
+import jobReducer from './slices/jobSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    job: jobReducer,
   },
 })
 
